621731d997e2d8d8ed0b2914f092e78e67b3100b
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-03-30 10:46:27 +0530

    LMS Client

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -7,8 +7,8 @@ export default function AboutLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10 lg:mt-60  mt-[800px]'>
-			<div className='  text-center justify-center  w-screen lg:mt-60 mt-[800px]'>
+		<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10 lg:mt-60 '>
+			<div className='  text-center justify-center  w-screen lg:mt-60 mt-[1600px]'>
 				{children}
 			</div>
 		</section>
