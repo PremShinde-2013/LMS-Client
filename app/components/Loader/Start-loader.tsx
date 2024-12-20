4d9d38db3c7c7a59eed24ac427c90b41768f66e7
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-12-20 11:54:10 +0530

    Deleted Typing animation file

--- app/components/Loader/Start-loader.tsx
+++ app/components/Loader/Start-loader.tsx
@@ -1,6 +1,5 @@
 import { Spinner } from '@nextui-org/react';
 import React from 'react';
-import { TypingAnimation } from '../ui/typing-animation';
 import { NeonGradientCard } from '../ui/neonCard';
 
 type Props = {};
